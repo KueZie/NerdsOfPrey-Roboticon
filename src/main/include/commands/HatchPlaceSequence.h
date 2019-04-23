140d8f6e9fa4780c5575e5f8bcef936ee29d2e7c
Hunter Goram <huntergoram2@gmail.com>
2019-04-22 23:17:14 -0400

    Added hatch place and grab sequences.

---
+++ src/main/include/commands/HatchPlaceSequence.h
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/CommandGroup.h>
+#include <frc/WPILib.h>
+#include "commands/ExtendHatchSlider.h"
+#include "commands/ToggleHatchSlider.h"
+#include "commands/ReleaseHatch.h"
+
+class HatchPlaceSequence : public frc::CommandGroup {
+ public:
+  HatchPlaceSequence();
+};
